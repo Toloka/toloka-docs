27300a1045d727eb7766bcb6147088eff8c29d9a
Dmitry Lekomtsev <didolf@yandex-team.ru>
2022-12-27 14:06:24 +0100

    chore: test

---
+++ menu.ts
@@ -0,0 +1,41 @@
+const defaultMenu = [
+  {
+    title: "Test submenu",
+    subItems: [
+      { href: "/apps/add-task", title: "Add task" },
+      { href: "/apps/download-results", title: "Download results" }
+    ]
+  },
+  {
+    href: "/apps/glossary",
+    title: "Glossary",
+  },
+  {
+    href: "/apps/product-search-relevance",
+    title: "Product search relevance",
+  },
+  {
+    href: "/apps/quickstart",
+    title: "Quickstart",
+  },
+  {
+    href: "/apps/support",
+    title: "Support",
+  },
+  {
+    href: "/apps/troubleshooting",
+    title: "Troubleshooting",
+  },
+  {
+    href: "/apps/unwanted",
+    title: "Unwanted",
+  },
+  {
+    href: "/apps/view-progress",
+    title: "View progress",
+  },
+]
+
+export const menuVariants = {
+  defaultMenu
+}
