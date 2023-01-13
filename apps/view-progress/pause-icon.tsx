f431a0342b28eb46a7f0b3ad58951e87626f4a11
Dmitry Lekomtsev <didolf@yandex-team.ru>
2023-01-13 10:24:28 +0100

    chore: test

---
+++ apps/view-progress/pause-icon.tsx
@@ -0,0 +1,17 @@
+export const PauseIcon = () => (
+  <svg
+    style={{ marginBottom: -2 }}
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="none"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill="#7F8285"
+      fillRule="evenodd"
+      d="M4 1.002a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-12a1 1 0 0 0-1-1H4Zm6 0a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-12a1 1 0 0 0-1-1h-2Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
